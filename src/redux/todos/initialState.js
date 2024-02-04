6e53d36897bfca00a042dcbcbbd08677dcbb9bc8
Farjana0313 <farjanaakter178@gmail.com>
2024-02-04 13:43:58 +0600

    todo reducer add

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,15 @@
+
+
+export const initialState = [
+    {
+        id: 1,
+        text: 'Learn Redux',
+        status: true
+    },
+    {
+        id: 2,
+        text: 'Leaun React js',
+        status: false,
+        color: "red"
+    }
+]
