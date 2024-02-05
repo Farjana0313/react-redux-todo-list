135b9c4cd17fab18e313548cf3ae915953c225c1
Farjana0313 <farjanaakter178@gmail.com>
2024-02-05 09:06:38 +0600

    test

---
+++ src/redux/filters/reducer.js
@@ -0,0 +1,11 @@
+import { COLORCHANGED, STATUSCHANGED } from "./actionTypes";
+
+const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case STATUSCHANGED:
+        case COLORCHANGED:
+        default
+}
+
+}
