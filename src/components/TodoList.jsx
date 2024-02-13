d7292d720271992d1015c94299b58e66c3581cc2
Farjana0313 <farjanaakter178@gmail.com>
2024-02-13 09:35:26 +0600

    filter working

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,39 @@
 import { useSelector } from 'react-redux';
 import Todo from './Todo';
 
+
+
 export default function TodoList() {
     const todos = useSelector((state) => state.todos);
+    const filters = useSelector((state) => state.filters);
+    const filterByStatus = (todo) => {
+        const { status } = filters;
+        switch (status) {
+            case 'Completed':
+                return todo.completed;
+            case 'Incomplete':
+                return !todo.completed;
+            default:
+                return true;
+        }
+    }
+    const filterByColor = (todo) => {
+        const { colors } = filters;
+        if (colors.length > 0) {
+            return colors.includes(todo?.color);
+        }
+        return true;
+    };
     return (
         <>
             {/* todo list */}
             <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-                {todos.map((todo) => (
-                    <Todo key={todo.id} todo={todo} />
-                ))}
+                {todos
+                    .filter(filterByStatus)
+                    .filter(filterByColor)
+                    .map((todo) => (
+                        <Todo key={todo.id} todo={todo} />
+                    ))}
             </div>
         </>
     )
